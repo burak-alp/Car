451758a7ae3ef9ac35c00f031d56b3442e415c46
burak-alp <58063672+burak-alp@users.noreply.github.com>
2021-03-15 19:47:18 +0300

    Add project files.

---
+++ Business/Abstact/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstact
+{
+    public interface ICarService
+    {
+        List<Car> GetAll();
+    }
+}
---
+++ Entites/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using Entites.Abstact;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entites.Concrete
+{
+    public class Car:IEntity
+    {
+        public int CarId { get; set; }
+        public int BrandlId { get; set; }
+        public string Description { get; set; }
+        public int ModelYear { get; set; }
+        public int ColorId { get; set; }
+        public int DailyPrice { get; set; }
+
+
+    }
+}
